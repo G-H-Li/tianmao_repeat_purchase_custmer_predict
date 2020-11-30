4f7eddb1555e7f2edec7f498db25549f320a3aeb
GH-Li <GH-Li@outlook.com>
2020-12-01 00:18:07 +0800

    complete this program

--- purchase_predict.py
+++ purchase_predict.py
@@ -3,11 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score
-from sklearn import model_selection
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestClassifier
 # import seaborn as sns
 
 
@@ -18,12 +14,12 @@ plt.rcParams["font.sans-serif"] = "SimHei"  # 解决中文乱码
 def preprocess():
     # read data
     print("start load data...")
-    train_data = pd.read_csv(r".\dataset\train_format1.csv")
     user_info = pd.read_csv(r".\dataset\user_info_format1.csv")
     user_log = pd.read_csv(r".\dataset\user_log_format1.csv", dtype={'time_stamp': 'str'})
     print("data loaded!")
     print('-' * 50)
 
+    print("start process train data to extract features...")
     # check and repair error data for user_info
     # user_info.info()
     print("start fix error data...")
@@ -40,19 +36,15 @@ def preprocess():
 
     # merge table to get feature
     # just to connect user_id, merchant_id with other param
-    print("start process train data to extract features...")
-    # First: connect with age_range, gender
-    train_data = pd.merge(train_data, user_info, on="user_id", how="left")  # .fillna(method='ffill')
-    train_data.head()
-    train_data.info()
+
     # Second: connect with total_logs
     total_logs_tmp = user_log.groupby([user_log["user_id"], user_log["seller_id"]]) \
         .count() \
         .reset_index()[["user_id", "seller_id", "item_id"]]
     total_logs_tmp.rename(columns={"seller_id": "merchant_id", "item_id": "total_logs"}, inplace=True)
-    train_data = pd.merge(train_data, total_logs_tmp, on=["user_id", "merchant_id"], how="left")
-    train_data.head()
-    train_data.info()
+    print("extract total_logs feature success!")
+    print('-' * 50)
+
     # Third: connect with item_ids
     item_ids_tmp = user_log.groupby([user_log["user_id"], user_log["seller_id"], user_log["item_id"]]) \
         .count() \
@@ -61,9 +53,9 @@ def preprocess():
         .count() \
         .reset_index()
     item_ids_tmp.rename(columns={"seller_id": "merchant_id", "item_id": "item_ids"}, inplace=True)
-    train_data = pd.merge(train_data, item_ids_tmp, on=["user_id", "merchant_id"], how="left")
-    train_data.head()
-    train_data.info()
+    print("extract item_ids feature success!")
+    print('-' * 50)
+
     # Fourth: connect with category
     categories_temp = user_log.groupby([user_log["user_id"], user_log["seller_id"], user_log["cat_id"]]) \
         .count() \
@@ -72,9 +64,9 @@ def preprocess():
         .count() \
         .reset_index()
     categories_temp.rename(columns={"seller_id": "merchant_id", "cat_id": "categories"}, inplace=True)
-    train_data = pd.merge(train_data, categories_temp, on=["user_id", "merchant_id"], how="left")
-    train_data.head()
-    train_data.info()
+    print("extract categories feature success!")
+    print('-' * 50)
+
     # Fifth: connect with browser_days
     browse_days_temp = user_log.groupby([user_log["user_id"], user_log["seller_id"], user_log["time_stamp"]]) \
         .count() \
@@ -83,10 +75,9 @@ def preprocess():
         .count() \
         .reset_index()
     browse_days_temp.rename(columns={"seller_id": "merchant_id"}, inplace=True)
-    train_data = pd.merge(train_data, browse_days_temp, on=["user_id", "merchant_id"], how="left")
-    train_data.head()
-    train_data.info()
-    # TODO: connect with the times per six months
+    print("extract browser_days feature success!")
+    print('-' * 50)
+
     # Sixth: connect with click_count, shopping_carts, purchase_count, favourite_count
     one_clicks_temp = user_log.groupby([user_log["user_id"], user_log["seller_id"], user_log["action_type"]]) \
         .count() \
@@ -100,37 +91,71 @@ def preprocess():
         .sum() \
         .reset_index()
     four_features = four_features.drop(["action_type", "times"], axis=1)
-    train_data = pd.merge(train_data, four_features, on=["user_id", "merchant_id"], how="left")
-    train_data = train_data.fillna(method="ffill")
-    train_data.head()
-    train_data.info()
+    print("extract four_features feature success!")
+    print('-' * 50)
     print("feature extraction success!")
-    print("saving train data...")
-    train_data.to_csv(path_or_buf=r".\dataset\train_data.csv", index=False)
+    return user_info, total_logs_tmp, item_ids_tmp, categories_temp, browse_days_temp, four_features
+
+
+def create_dataset(dataset_path, user_info, total_logs, item_ids, categories, browse_days, four_features, save_path):
+    print("start create dataset...")
+    data = pd.read_csv(dataset_path)
+    data = pd.merge(data, user_info, on="user_id", how="left")
+    data = pd.merge(data, total_logs, on=["user_id", "merchant_id"], how="left")
+    data = pd.merge(data, item_ids, on=["user_id", "merchant_id"], how="left")
+    data = pd.merge(data, categories, on=["user_id", "merchant_id"], how="left")
+    data = pd.merge(data, browse_days, on=["user_id", "merchant_id"], how="left")
+    data = pd.merge(data, four_features, on=["user_id", "merchant_id"], how="left")
+    data = data.fillna(method="bfill").fillna(method="ffill")
+    data.info()
+    print("saving data...")
+    data.to_csv(path_or_buf=save_path, index=False)
     print("saved")
     print('-' * 50)
 
 
-if __name__ == "__main__":
-    if not os.path.exists(r".\dataset\train_data.csv"):
-        preprocess()
-
-    print('load train data...')
-    train = pd.read_csv(r".\dataset\train_data.csv")
-    test = pd.read_csv(r".\dataset\test_format1.csv")
-    # start train
-    # prepare data
-    print("prepare data...")
-    X = train.drop(['user_id', 'merchant_id', 'label'], axis=1)
-    Y = train['label']
+def train_forest(data):
+    X = data.drop(['user_id', 'merchant_id', 'label'], axis=1)
+    Y = data['label']
     X.info()
     x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=10)
     print("start train...")
-    from sklearn.ensemble import RandomForestClassifier
     forest = RandomForestClassifier(n_estimators=10, random_state=2)
     forest.fit(x_train, y_train)
-    Predict_proba = forest.predict_proba(x_test)
-    print(Predict_proba[:])
+    predict_prob = forest.predict_proba(x_test)
+    print(predict_prob[:])
     print("Accuracy on training set: {:.3f}".format(forest.score(x_train, y_train)))
     print("Accuracy on test set: {:.3f}".format(forest.score(x_test, y_test)))
+    return forest
+
+
+def predict(data, model):
+    x = data.drop(['user_id', 'merchant_id', 'prob'], axis=1)
+    x.info()
+    print("start predict...")
+    predict_prob = model.predict_proba(x)
+    data['prob'] = predict_prob[:, 1]
+    choose = ['user_id', 'merchant_id', 'prob']
+    result = data[choose]
+    result.to_csv(path_or_buf=r".\dataset\prediction.csv", index=False)
+    print("predict end!")
+
+
+if __name__ == "__main__":
+    train_path = r".\dataset\train_data.csv"
+    test_path = r".\dataset\test_data.csv"
+    if not (os.path.exists(train_path) and os.path.exists(test_path)):
+        user_info, total_logs, item_ids, categories, browse_days, four_features = preprocess()
+        if not os.path.exists(train_path):
+            create_dataset(r".\dataset\train_format1.csv", user_info, total_logs, item_ids, categories, browse_days, four_features, train_path)
+        if not os.path.exists(test_path):
+            create_dataset(r".\dataset\test_format1.csv", user_info, total_logs, item_ids, categories, browse_days, four_features, test_path)
+
+    print('load train data...')
+    train_data = pd.read_csv(r".\dataset\train_data.csv")
+    test_data = pd.read_csv(r".\dataset\test_data.csv")
+    # start train
+    model = train_forest(train_data)
+    # start predict
+    predict(test_data, model)
 
